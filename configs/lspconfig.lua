093512c208b903ba6242cc90d2deff1767673097
Pierre V. Baudin <pierre@bellwethercoffee.com>
2023-11-06 11:05:19 -0800

    added switch header source binding

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -4,7 +4,7 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require "lspconfig"
 
 -- if you just want default config for the servers then put them in a table
-local servers = { "html", "cssls", "tsserver", "clangd", "jedi-language-server" }
+local servers = { "html", "cssls", "tsserver", "clangd"}
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
@@ -13,6 +13,6 @@ for _, lsp in ipairs(servers) do
   }
 end
 
-lspconfig.jedi_language_server.setup{}
+-- lspconfig.jedi_language_server.setup{}
 -- 
 -- lspconfig.pyright.setup { blabla}
